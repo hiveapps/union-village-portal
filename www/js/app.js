5daa35156a31ac4b44f833459a78ba3663b1e480
atans2468 <austintans7@gmail.com>
2016-07-05 14:50:52 -0700

    fixing ui-router bug upon state change

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,13 @@
 var unionVillage = angular.module('unionVillage', ['ui.router', 'unionVillage.controllers', 'unionVillage.services', 'unionVillage.directives', 'firebase']);
 
+
+unionVillage.run(function($rootScope) {
+    $rootScope.$on('$stateChangeSuccess', function() {
+        document.body.scrollTop = document.documentElement.scrollTop = 0;
+    });
+});
+
+
 unionVillage.config(function($stateProvider, $urlRouterProvider) {
       
   $stateProvider
